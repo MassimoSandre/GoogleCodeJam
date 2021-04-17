4a2402a3e57a4f7009cdc86fe53a28286818a5cb
Massimo Sandretti <massimo.sandretti@gmail.com>
2021-04-17 14:11:41 +0200

    Setting up repository

---
+++ Round 1A/Pattern_Matching/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t,n;
+    cin >> t;
+    for(int i = 0; i < t;i++) {
+        cin >> n;
+        vector<string> v(n);
+        string ris;
+        vector<int> len(n);
+        for(int j = 0; j < n;j++) {
+            cin >> v[j];
+            len[j] = v[j].length();
+        }
+        for(int j =1; j < n;j++) {
+
+        }
+    }
+    return 0;
+}
